053d03ce9f33b3dd4d5d1168a9d3f5efdade8822
Omid Daliri <omiidnk02@gmail.com>
2025-12-07 18:19:02 +0330

    build dashboard

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   plugins: [react(), tailwindcss()],
   base: "/react-admin-dashboard/",
 });
